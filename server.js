870e4f231a2f42531d929cc1d6010d1dcc2ddea7
KusKusVenom <berkanat07@gmail.com>
2025-12-28 23:52:42 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const weatherRoutes = require("./routes/weather");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/weather", weatherRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
